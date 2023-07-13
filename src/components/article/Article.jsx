4df1d1376e4c534cea41bc92aa2534768fb08d38
Michelle Orbezo <michelleorbezo@Michelles-MacBook-Air.local>
2023-07-13 17:11:42 -0400

    blog and footer containers, article component added

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -4,11 +4,18 @@ import './article.css';
 const Article = ({ imgUrl, date, title }) => {
   return (
     <div className='gpt3__blog-container_article'>
-      <div className='gpt__blog-container_article-image'>
-        <img src={imgUrl} alt='blog image' />
+      <div className='gpt3__blog-container_article-image'>
+        <img src={imgUrl} alt='blog' />
+      </div>
+      <div className='gpt3__blog-container_article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
       </div>
     </div>
   )
 }
 
-export default Article
+export default Article;
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -11,7 +11,7 @@ const Blog = () => {
       </div>
       <div className='gpt3__blog-container'>
         <div className='gpt3__blog-container_groupA'>
-          <Article imgUrl={blog01} date='Sep 26, 2021'/>
+          <Article imgUrl={blog01} date='Sep 26, 2021' title='GPT-3 and Open  AI is the future. Let us exlore how it is?'/>
         </div>
         <div className='gpt3__blog-container_groupB'>
           <Article imgUrl={blog02} date='Sep 26, 2021' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
@@ -24,4 +24,4 @@ const Blog = () => {
   )
 }
 
-export default Blog
+export default Blog;
